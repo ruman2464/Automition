9c5b8b7bdab23e4a3725f606db2d0a1c1ed62af0
Rubel <bittechcorp@gmail.com>
2013-10-26 21:55:11 -0400

    i have added one test class file

---
+++ src/test/java/AutomationTest.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class AutomationTest 
+{
+FirefoxDriver driver = new FirefoxDriver();
+	
+	@Before
+	public void start()
+	{
+		driver.get("http://www.facebook.com");
+	}
+	
+	@After
+	public void end()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void login()
+	{
+		driver.findElement(By.id("email")).sendKeys("qachithi@gmail.com");
+	}
+
+}
