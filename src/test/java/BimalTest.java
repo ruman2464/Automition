ac9c3e743ce0b141918f5ac876644e83dbf98426
Bimal <bimaldas551@yahoo.com>
2013-10-30 20:25:39 -0400

    modified

--- src/test/java/BimalTest.java
+++ src/test/java/BimalTest.java
@@ -1,13 +1,36 @@
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class BimalTest 
 {
-	
-	//This is new  project
+	FirefoxDriver driver = new FirefoxDriver();
+	//Thanks Everybody
 	
 	@Before
 	public void openBrowser()
+	{
+		driver.get("http://www.facebook.com");
+	}
+	@After
+	public void end() throws Exception
+	{
+		Thread.sleep(5000);
+		driver.quit();
+	}
+	@Test
+	public void login()
+	{
+		driver.findElement(By.id("email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("xyz567");
+		driver.findElement(By.id("u_0_c")).click();
+		
+	}
+	@Test
+	public void signUp()
 	{
 		
 	}
